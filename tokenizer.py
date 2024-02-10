1d920dbb1eb9a77d7a53cfaddc2496548e483b88
ShifengHong <shifengh@uci.edu>
2024-02-10 11:46:13 -0800

    Remove repeated function

--- tokenizer.py
+++ tokenizer.py
@@ -7,12 +7,6 @@ def yieldToken(text:str):
     pattern = re.compile(r'[a-zA-Z0-9]+', re.IGNORECASE)  # pattern being any sequence of alphanumeric character
     return (token.lower() for token in pattern.findall(text))  # yield lowercase token
 
-#Given a list of tokens and a freq_dict, update the word count based on the word tokens
-def updateWordFrequencies(tokens, freq_dict)-> None:
-    for token in tokens:
-        freq_dict[token] = freq_dict.get(token, 0) + 1 # if new to dict, assign 1, if not, increment
-    
-
 # n being size of frequencies dictionary, O(n), traverse through dictionary item (list) and print each O(1) operation
 def printFrequencies(frequencies):
     for (token, count) in sorted(frequencies.items(), key=lambda item: item[1],reverse=True):  # sort each pair by value

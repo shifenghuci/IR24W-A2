eca61ad1a3261b7145a25d3799818f0a626f27b6
ShifengHong <shifengh@uci.edu>
2024-02-14 15:03:37 -0800

    minor fix on function definition

--- scraper.py
+++ scraper.py
@@ -4,10 +4,12 @@ import lxml.html
 from urllib.parse import urlparse
 from typing import Generator
 from utils.response import Response
-import requests
+
 
 def get_words(resp: Response) -> list[str]:
-    # return words (as defined as sequence of alphanumeric string) from the resp
+    """
+    return words (as defined as sequence of alphanumeric string, including apstrophies) from the html string
+    """
     html_string = resp.raw_response.content
     tree = lxml.html.fromstring(html_string)
     pattern = r"\w+'?\w*"  # find any alphanumeric sequence, including apostrophe
@@ -16,7 +18,9 @@ def get_words(resp: Response) -> list[str]:
 
 
 def update_word_dict(resp: Response) -> None:
-    # update a shelves file about word frequencies
+    """
+    update a shelves file about word frequencies
+    """
     stop_words = {}
     with open('stopWords.txt', 'r') as f:
         stop_words = {word.strip() for word in f.readlines()}
@@ -34,7 +38,9 @@ def scraper(url: str, resp: Response) -> list[str]:
 
 
 def extract_next_links(url: str, resp: Response) -> Generator[str, None, None]:
-    # yield links extracted from the url, ensure that link yielded is absolute url
+    """
+    yield links extracted from the url, ensure that link yielded is absolute url
+    """
     hrefs = lxml.html.fromstring(resp.raw_response.content).xpath("//a/@href")
     for link in hrefs:
         if link.rstrip("/") == url or link == url:
@@ -49,6 +55,7 @@ def extract_next_links(url: str, resp: Response) -> Generator[str, None, None]:
             yield f'{url.rstrip("/")}{link}'  # remove ending / to avoid wrong concatenation
         else:
             yield link
+
 
 def is_valid(url: str) -> True | False:
     # return True for url we want to crawl, False for url we don't want to crawl
